aee95a33e7119243dcb9f3adb2c10eeb9f297e2f
DuncanCos <costes.duncan@gmail.com>
2021-04-19 14:51:15 +0200

    final build i think

---
+++ BirthDayDataBases.py
@@ -0,0 +1,71 @@
+import sqlite3
+import os
+
+
+def newLine():
+    nom = input("name of the personn ?")
+    date = input("day of birth ?")
+    cursor.execute("""
+    INSERT INTO users(name, date) VALUES(?, ?)""", (nom, date))
+    #cursor.close()
+
+
+def showTab():
+    cursor.execute("""SELECT id, name, date FROM users""")
+    for row in cursor:
+        print('{0} : {1}, {2}'.format(row[0], row[1], row[2]))
+    #cursor.close()
+
+def reset():
+    cursor.execute("DROP TABLE users")
+    conn.commit()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+         id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+         name VARCHAR(20),
+         date VARCHAR(20)
+    )
+    """)
+    conn.commit()
+
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+     name VARCHAR(20),
+     date VARCHAR(20)
+)
+""")
+
+clear = lambda: os.system('cls')
+choice=0
+while choice!='4':
+    print("what do you want to do ?")
+    print("1/ add anniv")
+    print("2/ show anniv")
+    print("3/ reset")
+    print("4/ quit")
+
+    choice = input("choice")
+
+    if(choice=='1'):
+        clear()
+        newLine()
+    elif(choice=='2'):
+        clear()
+        showTab()
+    elif(choice=='3'):
+        clear()
+        reset()
+    else:
+        print("put a good number")
+
+
+conn.commit()
+
+
+
+
+print("salut")
